bf52f60afad56a54d1e2503c76f9a75495d94679
yogsvn <yogsvn@87c15dda-f05d-4f0a-bada-50fc918e8268>
2016-04-10 08:52:53 +0000

    added makeJSON function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
 //
 $(document ,":)", function(){
 	var app = new json_builder_app("#jsonEditor");
+	var jObj = {
+		project:{
+			name : 'jsonmaker',
+			version : "1.1"
+		},
+		developer:{
+			name:{
+				firstName:'yogesh',
+				lastName:'jagdale'
+			},
+			age:24,
+			hobies:[
+				"reading",
+				"programming"
+			]
+		},
+		message:"Make your JSON while having your Coffee!"
+	}
 	json = app.json;
+	json.makeJSON(jObj);
 	$(".stringify","+={click}",function(){
 		var json = makeStringify();
 		if(json){
 			var rows = json.split(String.fromCharCode(10)).length;
-			$(".result","&x{hide}.e{innerHTML=}.>+{0}.@{1}",[json,{rows:rows}]);
+			$(".result","&x{hide}.e{value=''}.e{0}.@{1}",[{value:json},{rows:rows}]);
 		}
-	});
+	})[0].click();
 
 	$(".log","+={click}", function(){
 		var json = makeStringify();
