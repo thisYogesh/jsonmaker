36494348d00ed9673bc902da49e23447828fd3ac
yogsvn <yogsvn@87c15dda-f05d-4f0a-bada-50fc918e8268>
2017-03-25 13:50:07 +0000

    changing file location and allocation

---
+++ jsonbuilder/website/js/app.js
@@ -0,0 +1,20 @@
+//
+function json_builder_app(editor_selector){
+	if(!editor_selector)return false;
+	this.jsonEditors_container = editor_selector;
+	this.jsonEditor;
+	this.createJsonBuilder();
+}
+json_builder_app.prototype.createJsonBuilder = function(){
+	// actual view
+	var editorHtml = "<div class='json_editor'></div>";
+	this.jsonEditor = $(editorHtml).get(0);
+	$(this.jsonEditors_container).append(this.jsonEditor);
+	this.json = new json_object({
+		parent_el : this.jsonEditor
+	});
+}
+
+isNum = function(val){
+	return !isNaN(val);
+}
